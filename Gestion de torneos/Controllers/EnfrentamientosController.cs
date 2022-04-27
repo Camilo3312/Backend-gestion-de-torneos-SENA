d6fe5763031433ebd06ff27dd464a8e3b8ad9af3
camilo3312 <ccvelasquez531@misena.edu.co>
2022-04-26 22:25:21 -0500

    se añade metodo de partido

--- Gestion de torneos/Controllers/EnfrentamientosController.cs
+++ Gestion de torneos/Controllers/EnfrentamientosController.cs
@@ -25,7 +25,7 @@ namespace Gestion_de_torneos.Controllers
         public IActionResult Get(int id)
         {
             string query = @"
-                            select pd.id, pd.fechaencuentro, e.id as idequipo1, substring_index(GROUP_CONCAT(e.id),',', -1) as idequipo2, e.nombreequipo as equipo1, substring_index(GROUP_CONCAT(e.nombreequipo),',', -1) as equipo2, e.jornada ,t.nombretorneo , pd.idganador, l.id as idliguilla
+                            select pd.id, pd.fechaencuentro, e.id as idequipo1, substring_index(GROUP_CONCAT(e.id),',', -1) as idequipo2, e.nombreequipo as equipo1, substring_index(GROUP_CONCAT(e.nombreequipo),',', -1) as equipo2, e.jornada ,t.nombretorneo , pd.idganador, (select e.nombreequipo from equipos e where e.id = pd.idganador) as equipoganador, l.id as idliguilla
                             from equipos_partidos ep inner join partidos pd on ep.partido = pd.id
                             right join equipos e on ep.equipo = e.id
                             inner join liguilla l on e.liguilla = l.id
@@ -35,6 +35,14 @@ namespace Gestion_de_torneos.Controllers
                            ";
             var result = _db.Get(query, new { id = id });
             return Ok(result);
+        }
+
+        [HttpGet("partido/{idpartido}")]
+        public IActionResult GetPartido(int idpartido)
+        {
+            string query = $"select  pa.id as idJugador,pa.nombre as nombreJugador,e.id as idEquipo,e.nombreequipo as nombreEquipo from equipos e, partidos p, equipos_partidos ep, participantes pa  where ep.equipo=e.id and ep.partido=p.id and pa.equipo=e.id and partido={idpartido};";
+            string result1 = _db.ConvertDataTabletoString(query);
+            return Ok(result1);
         }
     }
 }
